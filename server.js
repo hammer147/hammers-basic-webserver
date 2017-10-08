b0514e209d4d36ac28faec912c1ee4f90177ae51
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2017-10-08 12:57:48 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.set('view engine', 'hbs');
+
+// app.use((req, res, next) => {
+// 	res.render('maintenance');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+	const now = new Date().toString();
+	const log = `${now}: ${req.method} ${req.url}`;
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if(err) {
+			console.error('Unable to append to server.log');
+		}
+	});
+	console.log(log);
+	next();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to my website!'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'Unable to handle request'
+	});
+});
+
+app.listen(3000);
