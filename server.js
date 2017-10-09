1d343a090bf39ee9a9f1c16958a854bd9b5ff84f
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2017-10-09 13:59:26 +0200

    Initial Commit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -17,34 +19,34 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-	const now = new Date().toString();
-	const log = `${now}: ${req.method} ${req.url}`;
-	fs.appendFile('server.log', log + '\n', (err) => {
-		if(err) {
-			console.error('Unable to append to server.log');
-		}
-	});
-	console.log(log);
-	next();
+    const now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.error('Unable to append to server.log');
+        }
+    });
+    console.log(log);
+    next();
 });
 
 app.get('/', (req, res) => {
-	res.render('home.hbs', {
-		pageTitle: 'Home Page',
-		welcomeMessage: 'Welcome to my website!'
-	});
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website!'
+    });
 });
 
 app.get('/about', (req, res) => {
-	res.render('about.hbs', {
-		pageTitle: 'About Page'
-	});
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
 });
 
 app.get('/bad', (req, res) => {
-	res.send({
-		errorMessage: 'Unable to handle request'
-	});
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
 });
 
-app.listen(3000);
+app.listen(port, () => `Server is up on port ${port}`);
